63e86d144098e3f17d42640226b91ff7f7088021
Durun <negiigainuki@gmail.com>
2022-05-18 00:14:27 +0900

    type definition of ArenaInfo

---
+++ src/wasmMain/kotlin/com/github/durun/screeps/arena/ArenaInfo.kt
@@ -0,0 +1,10 @@
+package com.github.durun.screeps.arena
+
+object ArenaInfo {
+    val name: String = TODO()
+    val level: Int = TODO()
+    val season: String = TODO()
+    val ticksLimit: Int = TODO()
+    val cpuTimeLimit: Nothing = TODO()
+    val cpuTimeLimitFirstTick: Nothing = TODO()
+}
