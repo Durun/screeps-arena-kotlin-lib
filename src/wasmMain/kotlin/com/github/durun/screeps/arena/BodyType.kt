e929dbf4ef7d5b1117e2651478029453543d8427
Durun <negiigainuki@gmail.com>
2022-05-21 12:50:01 +0900

    add BodyType description

--- src/wasmMain/kotlin/com/github/durun/screeps/arena/BodyType.kt
+++ src/wasmMain/kotlin/com/github/durun/screeps/arena/BodyType.kt
@@ -1,11 +1,48 @@
 package com.github.durun.screeps.arena
 
+/**
+ * cost 50
+ * Decreases fatigue by 2 points per tick.
+ */
 val MOVE = BodyType.MOVE
+
+/**
+ * cost	100
+ * Harvests 2 energy units from a source per tick.
+ * Builds a structure for 5 energy units per tick.
+ * Dismantles a structure for 50 hits per tick.
+ */
 val WORK = BodyType.WORK
+
+/**
+ * cost 50
+ * Can contain up to 50 resource units.
+ */
 val CARRY = BodyType.CARRY
+
+/**
+ * cost 80
+ * Attacks another creep/structure with 30 hits per tick in a short-ranged attack.
+ */
 val ATTACK = BodyType.ATTACK
+
+/**
+ * cost 150
+ * Attacks another single creep/structure with 10 hits per tick in a long-range attack up to 3 squares long.
+ * Attacks all hostile creeps/structures within 3 squares range with 1-4-10 hits (depending on the range).
+ */
 val RANGED_ATTACK = BodyType.RANGED_ATTACK
+
+/**
+ * cost 250
+ * Heals self or another creep restoring 12 hits per tick in short range or 4 hits per tick at a distance.
+ */
 val HEAL = BodyType.HEAL
+
+/**
+ * cost 10
+ * No effect, just additional hit points to the creep's body.
+ */
 val TOUGH = BodyType.TOUGH
 
 enum class BodyType(val int: Int) {
